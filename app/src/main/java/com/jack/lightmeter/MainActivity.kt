0bbff8077a8d87a74fc731db346c2f7ce59584d1
Jack Donnelly <jack@jackdonnelly.dev>
2022-10-25 11:45:15 +0100

    Working on adding the sensor connection for reading the light level

--- app/src/main/java/com/jack/lightmeter/MainActivity.kt
+++ app/src/main/java/com/jack/lightmeter/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.jack.lightmeter
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.jack.lightmeter.R.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    var sensorManager:SensorManager?= null
+    var sensor:Sensor?=null
+    var tv1:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager?.getDefaultSensor(Sensor.TYPE_LIGHT)
+        tv1 = findViewById(id.text)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause(){
+        super.onPause();
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event!!.values[0] > 40){
+            var lx:Float
+            lx = event!!.values[0]
+            tv1?.text = lx.toString() + "lx"
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
+
+
