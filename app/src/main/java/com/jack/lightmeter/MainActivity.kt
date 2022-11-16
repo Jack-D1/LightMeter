03c47dda4885d549dc34920258a5584587201f7c
Jack Donnelly <jack@jackdonnelly.dev>
2022-11-16 14:05:57 +0000

    Added spinners for selecting ISO and Aperture

--- app/src/main/java/com/jack/lightmeter/MainActivity.kt
+++ app/src/main/java/com/jack/lightmeter/MainActivity.kt
@@ -8,9 +8,9 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SeekBar
-import android.widget.Spinner
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 import com.jack.lightmeter.R.*
 import kotlin.math.abs
 import kotlin.math.log
@@ -24,8 +24,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     var tv3:TextView?=null
     var textConstant:TextView?=null
     var c:Int?=null
-    var ISODropdown:Spinner?=null
+    var ISO:Int?=null
+    var Aperture:Float?=null
     val ShutterSpeeds = listOf<Float>(8f,4f,2f,1f,0.5f,0.25f,0.125f,0.066f,0.033f,0.0166f,0.008f,0.004f, 0.002f, 0.001f,0.0005f,0.00025f,0.000125f)
+    val ISOs = listOf<String>("50", "100","200", "400", "800", "1600", "3200", "6400", "12800", "25600")
+    val Apertures = listOf<String>("1.0","1.2", "1.4", "2.0", "2.8", "3.2", "3.5", "4.0", "4.5", "5.0", "5.6", "6.3", "7.1", "8.0", "9.0", "10.0", "11.0", "13.0", "14.0", "16.0", "18.0", "20.0", "22.0","32.0")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
@@ -35,7 +38,37 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         tv2 = findViewById(id.text_lux)
         tv3 = findViewById(id.text_ev)
         textConstant = findViewById(id.text_constant)
-        ISODropdown = findViewById(id.ISO)
+        ISO = 50
+        Aperture = 1.0f
+        val spinnerISO = findViewById<Spinner>(id.ISO)
+        val spinnerAperture = findViewById<Spinner>(id.APERTURE)
+        if(spinnerISO != null){
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,ISOs)
+            spinnerISO.adapter = adapter
+            spinnerISO.onItemSelectedListener = object :AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    ISO = ISOs[position].toInt()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+        if(spinnerAperture != null){
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, Apertures)
+            spinnerAperture.adapter = adapter
+            spinnerAperture.onItemSelectedListener = object :AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Aperture = Apertures[position].toFloat()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
         c = 100
         var sb:SeekBar = findViewById(id.constant)
         sb.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
@@ -53,6 +86,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 c = sb.progress
             }
         })
+
     }
 
     override fun onResume() {
@@ -71,13 +105,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             var EV:Float
             lx = event!!.values[0]
             var ts:Float
-            ts = (((16f * 16f) * c!!)) / (lx * 100f)
-            EV = log(16.0 * 16.0 / ts.toDouble(),2.0).toFloat()
+            ts = (((Aperture!! * Aperture!!) * c!!)) / (lx * ISO!!.toFloat())
+            EV = log((Aperture!! * Aperture!!)/ ts.toDouble(),2.0).toFloat()
             var cloestTs: Float? = ShutterSpeeds.closestValue(ts)
             if(cloestTs!! >= 0.5f){
                 tv1?.text = cloestTs.toString() + " seconds"
             }else{
-                var denom:Float = 1f / cloestTs
+                var denom:Float = Math.ceil(1.0 / cloestTs).toFloat()
                 tv1?.text = "1/"+denom.toString() + "th of a second"
             }
 
