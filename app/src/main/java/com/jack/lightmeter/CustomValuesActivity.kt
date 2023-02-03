72dacbf47baa73a18c940b7045613dd185175e13
Jack Donnelly <jack@jackdonnelly.dev>
2023-02-03 10:03:01 +0000

    Beginnings of Agnostic custom values activity

---
+++ app/src/main/java/com/jack/lightmeter/CustomValuesActivity.kt
@@ -0,0 +1,11 @@
+package com.jack.lightmeter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CustomValuesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_values)
+    }
+}
